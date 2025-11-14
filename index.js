f49dfe25902ec16460d653406b2b25aa7bd04d15
Nguyen Hoang Phi Hung <nhph20049@gmail.com>
2025-11-14 21:42:08 +0700

    update cors

--- index.js
+++ index.js
@@ -17,21 +17,25 @@ const allowedOrigins = [
   "http://localhost:3006", // dev
   "https://booking-care-fe-8k8u.vercel.app", 
   "https://booking-care-fe-8k8u-git-main-hungs-projects-88cb76d9.vercel.app",
-  "https://booking-care-fe-8k8u-8atzjrhua-hungs-projects-88cb76d9.vercel.app"
+  "https://booking-care-fe-8k8u-8atzjrhua-hungs-projects-88cb76d9.vercel.app",
+  "https://bookingcare-be-eh6u.onrender.com"
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
+    // Cho phép request không có origin (Postman, server-side, Render internal)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
     }
+
+    return callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-}));
+}))
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
